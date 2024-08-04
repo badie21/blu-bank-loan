574ec3c0fb866ac910a695d8aa193118d34e6b21
badie21 <badie21fcb@gmail.com>
2024-08-04 05:00:08 +0330

    Fix: layout style

--- src/components/organisms/nav-bar/index.tsx
+++ src/components/organisms/nav-bar/index.tsx
@@ -1,15 +1,31 @@
 import Link from 'next/link';
 
+import { LoanPages, PublicPages } from 'types';
+
 const NavBar = () => {
   return (
-    <nav
-      className={
-        'h-16  p-6 flex justify-center items-center gap-6 text-center fixed top-0 right-0 left-0  bg-black bg-opacity-20 text-black font-bold '
-      }
-    >
-      <Link href={'/'} className={'p-2 bg-gray-500 min-w-40 text-white hover:text-gray-800 hover:bg-gray-400 rounded'}>
-        لیست تسهیلات فعال
-      </Link>
+    <nav className="bg-white h-16 absolute top-0 left-0 right-0 border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="font-light flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <Link href={LoanPages.SelectLoanType}>
+              <li>
+                <button
+                  type="button"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  درخواست تسهیلات جدید
+                </button>
+              </li>
+            </Link>
+            <Link href={PublicPages.Dashboard}>
+              <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                لیست تسهیلات
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
--- src/components/templates/layout/index.tsx
+++ src/components/templates/layout/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 const Layout: FC<Props> = ({ children }) => {
   return (
     <div className="flex min-h-screen flex-col items-center gap-5  bg-gray-300">
-      <div className={'w-[390px] bg-white h-screen pt-24 flex justify-center '}>
+      <div className={'w-[390px] bg-white h-screen overflow-auto pt-24 flex justify-center relative'}>
         <NavBar />
         <main className={'w-full p-4'}>{children}</main>
       </div>
