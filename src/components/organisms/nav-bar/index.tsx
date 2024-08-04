abe5a42e5efbf08401ae6c6377483afa6feeea34
badie21 <m.badneneshin@tejaratshayan.com>
2024-08-04 00:22:40 -0700

    Fix: navbar style

--- src/components/organisms/nav-bar/index.tsx
+++ src/components/organisms/nav-bar/index.tsx
@@ -4,28 +4,22 @@ import { LoanPages, PublicPages } from 'types';
 
 const NavBar = () => {
   return (
-    <nav className="bg-white h-16 absolute top-0 left-0 right-0 border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-light flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <Link href={LoanPages.SelectLoanType}>
-              <li>
-                <button
-                  type="button"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  درخواست تسهیلات جدید
-                </button>
-              </li>
-            </Link>
-            <Link href={PublicPages.Dashboard}>
-              <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                لیست تسهیلات
-              </li>
-            </Link>
-          </ul>
-        </div>
-      </div>
+    <nav className=" h-16 absolute top-0 left-0 right-0 border-gray-200 bg-gray-900">
+      <ul className="flex h-full items-center justify-center gap-2">
+        <Link href={LoanPages.SelectLoanType}>
+          <li>
+            <button
+              type="button"
+              className="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              درخواست تسهیلات جدید
+            </button>
+          </li>
+        </Link>
+        <Link href={PublicPages.Dashboard} className={'text-white'}>
+          <li className=" bg-transparent">لیست تسهیلات</li>
+        </Link>
+      </ul>
     </nav>
   );
 };
