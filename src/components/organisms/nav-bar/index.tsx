ab3cc0193b974485b8ba6ebbfe51b49d6be43b7f
badie21 <badie21fcb@gmail.com>
2024-08-04 03:46:55 +0330

    Fix: navbar style

--- src/components/organisms/nav-bar/index.tsx
+++ src/components/organisms/nav-bar/index.tsx
@@ -2,15 +2,13 @@ import Link from 'next/link';
 
 const NavBar = () => {
   return (
-    <nav className={'h-16 w-full p-6 flex justify-center items-center gap-6 text-center  bg-black bg-opacity-20 text-black font-bold '}>
+    <nav
+      className={
+        'h-16  p-6 flex justify-center items-center gap-6 text-center fixed top-0 right-0 left-0  bg-black bg-opacity-20 text-black font-bold '
+      }
+    >
       <Link href={'/'} className={'p-2 bg-gray-500 min-w-40 text-white hover:text-gray-800 hover:bg-gray-400 rounded'}>
-        لیست وام‌های فعال
-      </Link>
-      <Link href={'/'} className={'p-2 bg-gray-500 min-w-40 text-white hover:text-gray-800 hover:bg-gray-400 rounded'}>
-        خانه
-      </Link>
-      <Link href={'/'} className={'p-2 bg-gray-500 min-w-40 text-white hover:text-gray-800 hover:bg-gray-400 rounded'}>
-        درباره ما
+        لیست تسهیلات فعال
       </Link>
     </nav>
   );
